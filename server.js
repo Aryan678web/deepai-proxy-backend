5d69b068da1ff32a2c86fb87aa0153edc0ab6b97
Aryan678web <aayanbhai2007@gmail.com>
2025-04-07 15:27:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ app.post('/convert', upload.single('image'), async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});￼Enter
+});
